6122eeefd4af7cc93aabf7570dfa127b08c6dad4
Simeon Grancharov <skgryncharov@gmail.com>
2023-10-18 14:25:06 +0300

    Lualine custom time

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -1,5 +1,13 @@
 require('lualine').setup({
   options = {
     theme = 'everforest'
+  },
+  sections = {
+    lualine_y = {
+      {
+        'datetime',
+        style = '%H:%M'
+      }
+    }
   }
 })
