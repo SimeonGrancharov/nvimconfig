8475e420245aefba55d66382b74966845e933728
Simeon Grancharo <simeongrancharov@Simeons-MacBook-Air.local>
2025-12-08 18:03:10 +0200

    add support for oxfmt

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,18 +1,37 @@
 return {
   "stevearc/conform.nvim",
   config = function()
+    local util = require("conform.util")
+
     require("conform").setup({
+      formatters = {
+        oxfmt = {
+          stdin = false,
+          command = util.from_node_modules("oxfmt"),
+          tmp_file = ".conform.$RANDOM.$FILENAME",
+          args = { "$FILENAME" }
+        }
+      },
+
       formatters_by_ft = {
         lua = { "stylua" },
         python = { "isort", "black" },
         rust = { "rustfmt", lsp_format = "fallback" },
-        javascript = { "prettier", stop_after_first = true },
-        javascriptreact = { "prettier", stop_after_first = true },
-        typescript = { "prettier", stop_after_first = true },
-        typescriptreact = { "prettier", stop_after_first = true },
-        css = { "prettier", stop_after_first = true },
-        scss = { "prettier", stop_after_first = true },
-        sass = { "prettier", stop_after_first = true },
+        -- javascript = { "prettier", stop_after_first = true },
+        -- javascriptreact = { "prettier", stop_after_first = true },
+        -- typescript = { "prettier", stop_after_first = true },
+        -- typescriptreact = { "prettier", stop_after_first = true },
+        css = { "oxfmt", "prettier", stop_after_first = true },
+        scss = { "oxfmt", "prettier", stop_after_first = true },
+        sass = { "oxfmt", "prettier", stop_after_first = true },
+        javascript = { "oxfmt", "prettier", stop_after_first = true },
+        typescript = { "oxfmt", "prettier", stop_after_first = true },
+        javascriptreact = { "oxfmt", "prettier", stop_after_first = true },
+        typescriptreact = { "oxfmt", "prettier", stop_after_first = true },
+        json = { "oxfmt", "prettier", stop_after_first = true },
+        jsonc = { "oxfmt" },
+        astro = { "oxfmt" },
+        markdown = { "oxfmt" },
       },
     })
 
