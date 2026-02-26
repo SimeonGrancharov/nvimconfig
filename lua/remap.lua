e00afbe79816ef12ee259e6e4a4d01b3acec26bc
simeon.grancharov <simeon.grancharov@cubbystorage.com>
2026-02-26 11:41:36 +0200

    ADd btop command

--- lua/remap.lua
+++ lua/remap.lua
@@ -48,6 +48,35 @@ keymap.set("t", "<Tab><Tab>", "<C-\\><C-n>")
 -- disable mouse scrolling
 vim.opt.mouse = ""
 
+-- btop floating terminal
+keymap.set("n", "<leader>bt", function()
+  local buf = vim.api.nvim_create_buf(false, true)
+  local width = math.floor(vim.o.columns * 0.9)
+  local height = math.floor(vim.o.lines * 0.9)
+  local row = math.floor((vim.o.lines - height) / 2)
+  local col = math.floor((vim.o.columns - width) / 2)
+
+  vim.api.nvim_open_win(buf, true, {
+    relative = "editor",
+    width = width,
+    height = height,
+    row = row,
+    col = col,
+    style = "minimal",
+    border = "rounded",
+  })
+
+  vim.fn.termopen("btop", {
+    on_exit = function()
+      if vim.api.nvim_buf_is_valid(buf) then
+        vim.api.nvim_buf_delete(buf, { force = true })
+      end
+    end,
+  })
+
+  vim.cmd("startinsert")
+end, { desc = "Open btop in floating window" })
+
 -- console.log snippet
 keymap.set("n", "<leader>cl", function()
     vim.api.nvim_put({ "console.log('[MONKA]', )" }, "l", true, false)
